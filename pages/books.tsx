ed69df5fdad73f0fb3f3a4d593e6cb1f536f8b63
akbar putra <ap975527@gmail.com>
2023-02-02 14:06:48 +0700

    create books page

--- pages/books.tsx
+++ pages/books.tsx
@@ -1,16 +1,77 @@
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 
+const data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
 export default function Home() {
 
-	return (
-		<>
-			<div className="">
-				<Header/>
-			</div>
+    return (
+        <>
+            <div className="">
+                <Header />
+            </div>
 
-			{/* footer */}
-			<Footer/>
-		</>
-	)
+            {/* books */}
+            <section className="py-10 bg-gray-50 sm:py-16 lg:py-24">
+                <div className="px-4 mx-auto sm:px-6 lg:px-8 max-w-7xl">
+                    <div className="flex items-end justify-between">
+                        <div className="flex-1 text-center lg:text-left">
+                            <h2 className="text-3xl font-bold leading-tight text-black sm:text-4xl lg:text-5xl">Books from millennial publisher</h2>
+                            <p className="max-w-xl mx-auto mt-4 text-base leading-relaxed text-gray-600 lg:mx-0">Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis.</p>
+                        </div>
+
+                        <div className="hidden lg:flex lg:items-center lg:space-x-3">
+                            <button type="button" className="flex items-center justify-center text-gray-400 transition-all duration-200 bg-transparent border border-gray-300 rounded w-9 h-9 hover:bg-blue-600 hover:text-white focus:bg-blue-600 focus:text-white">
+                                <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 19l-7-7 7-7" />
+                                </svg>
+                            </button>
+
+                            <button type="button" className="flex items-center justify-center text-gray-400 transition-all duration-200 bg-transparent border border-gray-300 rounded w-9 h-9 hover:bg-blue-600 hover:text-white focus:bg-blue-600 focus:text-white">
+                                <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 5l7 7-7 7" />
+                                </svg>
+                            </button>
+                        </div>
+                    </div>
+
+                    <div className="grid max-w-md grid-cols-2 gap-6 mx-auto mt-8 lg:mt-16 lg:grid-cols-4 lg:max-w-full">
+                        {data.map(d => (
+                            <div key={d} className="overflow-hidden bg-white rounded-md shadow-md">
+                                <div className="p-5">
+                                    <div className="relative">
+                                        <a href="#" title="" className="block aspect-w-4 aspect-h-3">
+                                            <img className="object-cover w-full h-full" src="https://cdn.rareblocks.xyz/collection/celebration/images/blog/2/blog-post-1.jpg" alt="" />
+                                        </a>
+                                    </div>
+                                    <p className="mt-5 text-md lg:text-xl font-semibold">
+                                        <a href="#" title="" className="text-black"> How to build coffee inside your home in 5 minutes. </a>
+                                    </p>
+                                    <a href="#" title="" className="inline-flex items-center justify-center px-5 py-2 text-sm font-semibold text-white rounded-full transition-all duration-200 bg-primary hover:bg-primary-dark focus:bg-primary-dark mt-5" role="button">Read more</a>
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+
+                    <div className="flex items-center justify-center mt-8 space-x-3 lg:hidden">
+                        <button type="button" className="flex items-center justify-center text-gray-400 transition-all duration-200 bg-transparent border border-gray-300 rounded w-9 h-9 hover:bg-blue-600 hover:text-white focus:bg-blue-600 focus:text-white">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 19l-7-7 7-7" />
+                            </svg>
+                        </button>
+
+                        <button type="button" className="flex items-center justify-center text-gray-400 transition-all duration-200 bg-transparent border border-gray-300 rounded w-9 h-9 hover:bg-blue-600 hover:text-white focus:bg-blue-600 focus:text-white">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 5l7 7-7 7" />
+                            </svg>
+                        </button>
+                    </div>
+                </div>
+            </section>
+
+
+            {/* footer */}
+            <Footer />
+        </>
+    )
 }
